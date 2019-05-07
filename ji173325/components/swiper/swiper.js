a3c89d827024d7dfdeaba7b01aa369fbc42eebf6
Zhangsy25 <48703867+Zhangsy25@users.noreply.github.com>
2019-05-07 11:31:05 +0800

    Add files via upload

--- ji173325/components/swiper/swiper.js
+++ ji173325/components/swiper/swiper.js
@@ -11,7 +11,13 @@ Component({
    * 组件的初始数据
    */
   data: {
-    
+    imgList: [{
+      imagePath: "/images/img1.jpg"
+    }, {
+      imagePath: "/images/img2.jpg"
+    }, {
+      imagePath: "/images/img3.jpg"
+    }]
   },
 
   /**
